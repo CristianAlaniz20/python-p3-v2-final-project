6942bbb950daf5928363a403058d1a829aee2f01
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-08-04 23:59:40 -0700

    Createclient.py, add __init__ methods and property methods to Client class

---
+++ lib/models/client.py
@@ -0,0 +1,45 @@
+from __init__ import CONN, CURSOR
+import re
+
+phone_number = r"(\d{3}-){2}\d{4}"
+phone_regex = re.compile(phone_number)
+
+class Client:
+    def __init__(self, first_name, last_name, phone_number, id=None):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        if isinstance(first_name, str) and len(first_name):
+            self._first_name = first_name
+        else:
+            raise ValueError("First name cannot be empty")
+    
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        if isinstance(last_name, str) and len(last_name):
+            self._last_name = last_name
+        else:
+            raise ValueError("Last name cannot be empty")
+
+    @property
+    def phone_number(self):
+        return self._phone_number
+    
+    @phone_number.setter
+    def phone_number(self, phone_number):
+        if phone_regex.fullmatch(phone_number):
+            self._phone_number = phone_number
+        else:
+            raise ValueError("Enter phone number with the following pattern: 555-555-5555")
