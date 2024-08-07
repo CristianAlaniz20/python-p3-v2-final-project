e0f5d10ab14cd3f2dd27e1803e5b6c3cdfd456a9
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-08-06 23:57:03 -0700

    add list_clients and select_client_by_enumerate_number helper methods. incorporateinto cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -2,26 +2,37 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    list_clients,
+    select_client_by_enumerate_number,
+    search_client_by_name
 )
 
 
 def main():
     while True:
-        menu()
+        main_menu()
         choice = input("> ")
-        if choice == "0":
+        if choice == "exit" or choice == "e":
             exit_program()
-        elif choice == "1":
-            helper_1()
+        elif choice == "view" or choice == "v":
+            list_clients()
+            view_all_clients_menu()
+        elif choice == "select" or choice == "s":
+            search_client_by_name()
         else:
             print("Invalid choice")
 
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+def main_menu():
+    print("Program started, please select an option:")
+    print("Type 'exit' or 'e' to exit the program")
+    print("Type 'view' or 'v' to view all clients")
+    print("Type 'select' or 's' to search for a client")
+
+def view_all_clients_menu():
+    print("Please select a client by corresponding number")
+    select_client_by_enumerate_number()
+
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,27 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+from models.client import Client
 
+spacing = "-----------------------------"
 
 def exit_program():
-    print("Goodbye!")
+    print("Program closed, goodbye!")
     exit()
+
+def list_clients():
+    clients = Client.get_all()
+    print("Clients list:")
+    print(spacing)
+    for i, client in enumerate(clients, start=1):
+        print(f"{i}. {client.first_name} {client.last_name}")
+    print(spacing)
+
+def select_client_by_enumerate_number():
+    number = input("client number> ")
+    try:
+        client = Client.find_by_id(int(number))
+        print(client.first_name) 
+    except:
+        print(f"Error: no client for number {number}")
+        select_client_by_enumerate_number()
+
