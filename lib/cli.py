a019fa98cd0c1a6623f2c16a33dad50cc33d5ab6
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-08-08 23:47:14 -0700

    edit select_client_by_enumerate_number(), main(), view_client_menu(). Create client_view(), client_view_menu(), update_client(), and delete_client()

--- lib/cli.py
+++ lib/cli.py
@@ -1,39 +1,76 @@
 # lib/cli.py
+import ipdb
 
 from helpers import (
     exit_program,
     list_clients,
     select_client_by_enumerate_number,
-    search_client_by_name
+    update_client,
+    delete_client
 )
 
+def invalid_input_message():
+    print("invalid input")
 
 def main():
     while True:
+        print("Program started!")
         main_menu()
         choice = input("> ")
-        if choice == "exit" or choice == "e":
+        if choice == "e":
             exit_program()
-        elif choice == "view" or choice == "v":
-            list_clients()
-            view_all_clients_menu()
-        elif choice == "select" or choice == "s":
+        elif choice == "v":
+            view_client_menu()
+        elif choice == "s":
             search_client_by_name()
         else:
-            print("Invalid choice")
+            invalid_input_message()
 
+def view_client_menu():
+    list_clients()
+    view_all_clients_menu()
+    choice = input("> ")
+    if choice == "exit" or choice == "e":
+            exit_program()
+    else:
+        if select_client_by_enumerate_number(choice):
+            print(select_client_by_enumerate_number(choice))
+            client_menu()
+        else:
+            print("Hi from inside else statement")
+
+def client_menu():
+    client_view_menu()
+    choice = input("> ")
+    if choice == "e":
+            exit_program()
+    elif choice == "u":
+        update_client()
+    elif choice == "d":
+        delete_client()
+    elif choice == "v":
+        pass
+    else:
+        invalid_input_message()
+        client_menu()
 
 def main_menu():
-    print("Program started, please select an option:")
-    print("Type 'exit' or 'e' to exit the program")
-    print("Type 'view' or 'v' to view all clients")
-    print("Type 'select' or 's' to search for a client")
+    print("Please select an option:")
+    print("Enter 'exit' or 'e' to exit the program")
+    print("Enter 'view' or 'v' to view all clients")
+    print("Enter 'select' or 's' to search for a client")
 
 def view_all_clients_menu():
-    print("Please select a client by corresponding number")
-    select_client_by_enumerate_number()
+    print("Enter 'p' to go to the previous menu")
+    print("Enter 'e' to exit the program")
+    print("Or select a client by entering their corresponding number")
 
-
+def client_view_menu():
+    print("Enter 'p' to go to the previous menu")
+    print("Enter 'e' to exit the program")
+    print("Enter 'u' to update client details")
+    print("Enter 'd' to delete this client")
+    print("Enter 'v' to view all the trailers this client has rented")
 
 if __name__ == "__main__":
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -8,20 +8,63 @@ def exit_program():
     print("Program closed, goodbye!")
     exit()
 
+def show_client_info(client):
+    print(client.first_name, client.last_name)
+    print(f"Phone number: {client.phone_number}")
+
 def list_clients():
     clients = Client.get_all()
     print("Clients list:")
-    print(spacing)
     for i, client in enumerate(clients, start=1):
         print(f"{i}. {client.first_name} {client.last_name}")
     print(spacing)
 
-def select_client_by_enumerate_number():
-    number = input("client number> ")
+def select_client_by_enumerate_number(choice):
     try:
-        client = Client.find_by_id(int(number))
-        print(client.first_name) 
+        client = Client.find_by_id(int(choice))
+        show_client_info(client)
+        print(spacing)
     except:
-        print(f"Error: no client for number {number}")
-        select_client_by_enumerate_number()
+        print(f"Error: no client found for {choice}")
+        print(spacing)
 
+def update_client(client):
+    first_name = input("Enter the new first name: ")
+    client.first_name = first_name
+    last_name = input("Enter the new last name: ")
+    client.last_name = last_name
+    phone_number = input("Enter the new phone number: ")
+    client.phone_number = phone_number
+    try:
+        client.update()
+        print("Succesfully updated!")
+        show_client_info(client)
+    except Exception as exc:
+        print("Error: ", exc)
+        print(spacing)
+
+def delete_client(client):
+    print("""
+    Are you sure you want to delete this client?
+    Enter 'yes' or 'no'""")
+    _input = input("> ")
+    if _input == "yes":
+        client.delete()
+    elif _input == "no":
+        print("Client NOT deleted")
+        show_client_info(client)
+    else:
+        print("Invalid input")
+
+    
+
+def search_client_by_name():
+    client = input("Type in client first and last name: ")
+    pass
+
+def list_trailers():
+    pass
+
+def search_trailer():
+    trailer = input("Type in trailer number: ")
+    pass
