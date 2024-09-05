b2cc972ba5c3fa6957507b2f9057bf92811b2733
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-09-04 19:05:36 -0700

    create property methods for Trailer class properties.

--- lib/models/trailer.py
+++ lib/models/trailer.py
@@ -1,4 +1,29 @@
+from .client import Client
+
 class Trailer:
-    def __init__(self, client_renting_trailer, id=None):
+    def __init__(self, client_renting_trailer, available=True, id=None):
         self.id = id
         self.client_renting_trailer = client_renting_trailer
+        self.available = available
+    
+    @property
+    def client_renting_trailer(self):
+        return self._client_renting_trailer
+
+    @client_renting_trailer.setter
+    def client_renting_trailer(self, client_renting_trailer):
+        if isinstance(client_renting_trailer, int) and Client.find_by_id(client_renting_trailer):
+            return self._client_renting_trailer = client_renting_trailer
+        else:
+            raise ValueError("No client found. Ensure that client exists.")
+
+    @property
+    def available(self):
+        return self._available
+    
+    @available.setter
+    def available(self, available):
+        if self.client_renting_trailer:
+            return self._available = False
+        else:
+            return self._available = True
